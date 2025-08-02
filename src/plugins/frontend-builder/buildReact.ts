2b3a1c778ff7fcf8d582e22e6992cf30c7afcc5b
Guilherme da Silva Benevides <git@gui.dev.br>
2025-08-02 15:27:31 -0300

    fix: sourcemap

--- src/plugins/frontend-builder/buildReact.ts
+++ src/plugins/frontend-builder/buildReact.ts
@@ -4,7 +4,7 @@ export const buildReact = async (options: ReactBuilderOptions) => {
   const output = await Bun.build({
     entrypoints: [options.entrypoint],
     minify: Bun.env.ENABLE_PRODUCTION_MODE === "true",
-    sourcemap: Bun.env.ENABLE_PRODUCTION_MODE !== "true" ? "inline" : false,
+    sourcemap: Bun.env.ENABLE_PRODUCTION_MODE !== "true" ? "inline" : "none",
   });
   return output.outputs[0];
 };
